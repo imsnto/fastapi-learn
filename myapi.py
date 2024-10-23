040538c5cb32c69124d9467bee23a3cee5743f47
Solaiman Hossain <slmnsnto@gmail.com>
2024-10-23 22:07:35 +0600

    add path parameter

--- myapi.py
+++ myapi.py
@@ -18,3 +18,13 @@ students = {
 @app.get("/")
 def index():
 	return {"name": "Solaiman"}
+	
+# path parameter
+@app.get("/get-student/{id}")
+def get_student(id: int = Path(..., description="The id of the student you want to view", gt=0, lt=3)):
+	student = students.get(id)
+	if student is None:
+		# Return an error response if the student is not found
+		raise HTTPException(status_code=404, detail="Student not found")
+	return student
+
