9132e4e375f9edd14a86fd7aa172589c7cdebf05
Solaiman Hossain <slmnsnto@gmail.com>
2024-10-23 23:23:00 +0600

    add post method and use BaseModel import from pydantic

--- myapi.py
+++ myapi.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException
 from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -7,22 +9,22 @@ students = {
 	1: {
 		"name": "Solaiman Hossain",
 		"age": 25,
-		"class": "B.Sc"
-	},
-	2: {
-		"name": "Hasan Mahmud",
-		"age": 24,
-		"class": "B.Sc"
+		"year": "class 12"
 	}
 }
 
+class Student(BaseModel):
+    name : str
+    age  : int
+    year : str
+
 @app.get("/")
 def index():
-	return {"name": "Solaiman"}
+	return students
 	
 # path parameter
 @app.get("/get-student/{id}")
-def get_student(id: int = Path(..., description="The id of the student you want to view", gt=0, lt=3)):
+def get_student(id: int = Path(..., description="The id of the student you want to view", gt=0, lt=20)):
 	student = students.get(id)
 	if student is None:
 		# Return an error response if the student is not found
@@ -39,3 +41,10 @@ def get_student(*, id : int, name : Optional[str] = None):
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"data": "Not Found"}
+
+@app.post("/create-student/{id}")
+def create_student(id:int, student:Student):
+    if id in students:
+        return {"Error": "Student Exist"}
+    students[id] = student
+    return students[id]
